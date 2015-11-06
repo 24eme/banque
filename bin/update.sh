bda506841ada52d403e28074fa981405f29bb7d2
Vincent LAURENT <vince.laurent@gmail.com>
2015-11-06 15:43:04 +0100

    Suelement pour un compte

--- bin/update.sh
+++ bin/update.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 . bin/config.inc
 
-for account_id in "${!ACCOUNTS_ID[@]}";
-do
-    cat $HISTORY_FILE $(boobank history $account_id -n 20 -f csv | awk -F ';' '{ print $2 ";" $6 ";" $9 ";" $1 ";" $3 ";" $4 ";" $5 ";" $8 }' | grep -E "^[0-9]+") | sort | uniq | sort -r > $HISTORY_FILE
-done
+cat $HISTORY_FILE $(boobank history $ACCOUND_ID -n 20 -f csv | awk -F ';' '{ print $2 ";" $6 ";" $9 ";" $1 ";" $3 ";" $4 ";" $5 ";" $8 }' | grep -E "^[0-9]+") | sort | uniq | sort -r > $HISTORY_FILE
 
 
