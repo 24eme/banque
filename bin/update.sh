0560b39bab351c504b981cafa29ef0f390cd8521
Vincent LAURENT <vince.laurent@gmail.com>
2015-11-06 16:34:46 +0100

    Tache de récupération de l'historique pour un compte

--- bin/update.sh
+++ bin/update.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
+
 . bin/config.inc
 
-cat $HISTORY_FILE $(boobank history $ACCOUND_ID -n 20 -f csv | awk -F ';' '{ print $2 ";" $6 ";" $9 ";" $1 ";" $3 ";" $4 ";" $5 ";" $8 }' | grep -E "^[0-9]+") | sort | uniq | sort -r > $HISTORY_FILE
+if [ ! -f $HISTORY_FILE ]
+then
+    echo "date;raw;amount;type;id;rdate;vdate;label" > $HISTORY_FILE
+fi
 
+boobank history $ACCOUNT_ID -n 20 -f csv | sed -r 's/^.+@//' | awk -F ';' '{ print $2 ";" $6 ";" $9 ";" $3 ";" $1 ";" $4 ";" $5 ";" $8 }' | grep -E "^[0-9]+" >> $HISTORY_FILE
+cat $HISTORY_FILE | sort| uniq | sort -r > $HISTORY_FILE.tmp
+cat $HISTORY_FILE.tmp > $HISTORY_FILE
+rm -f $HISTORY_FILE.tmp
 
+git diff $HISTORY_FILE
+
+git add $HISTORY_FILE
+git commit -m "Mise à jour des opérations" $HISTORY_FILE > /dev/null
+
+boobank list -f csv | grep $ACCOUNT_ID | sed -r 's/^.+@//' | awk -F ';' '{ print $2 ";" $5 ";" $6 ";" $3 ";" $4 ";" $1'} > $LIST_FILE.tmp
+
+if test $(wc -l $LIST_FILE.tmp | cut -d " " -f 1) -gt 0
+then
+    echo "label;balance;coming;currency;type;id" > $LIST_FILE
+    cat $LIST_FILE.tmp >> $LIST_FILE
+fi
+
+rm $LIST_FILE.tmp
+git diff $LIST_FILE
+
+git add $LIST_FILE
+git commit -m "Mise à jour du solde" $LIST_FILE > /dev/null
