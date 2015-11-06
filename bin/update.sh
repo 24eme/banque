bb00d6fb59554c0be68d074ebe2802639337442a
Vincent LAURENT <vince.laurent@gmail.com>
2015-11-06 15:35:11 +0100

    Initialisation

---
+++ bin/update.sh
@@ -0,0 +1,8 @@
+
+
+for account_id in "${!ACCOUNTS_ID[@]}";
+do
+    cat $HISTORY_FILE $(boobank history $account_id -n 20 -f csv | awk -F ';' '{ print $2 ";" $6 ";" $9 ";" $1 ";" $3 ";" $4 ";" $5 ";" $8 }' | grep -E "^[0-9]+") | sort | uniq | sort -r > $HISTORY_FILE
+done
+
+
