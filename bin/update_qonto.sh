9a4a86ec4534e0a5c0bffb64b389ec3f04820d68
Vincent LAURENT <vince.laurent@gmail.com>
2021-02-22 19:04:31 +0100

    Gestion des débit crédit

--- bin/update_qonto.sh
+++ bin/update_qonto.sh
@@ -7,7 +7,7 @@ git pull
 
 echo "date,\"raw\",amount,type,id,rdate,vdate,\"label\"" > $HISTORY_FILE".new"
 
-curl -s -H "Authorization: $QONTO_IDENTIFIANT:$QONTO_SECRET" "https://thirdparty.qonto.com/v2/transactions?slug=$QONTO_IDENTIFIANT&iban=$QONTO_IBAN" | jq '.transactions[]' | jq -c "[.settled_at,.label,.reference,.amount,.operation_type,\"$QONTO_COMPTEBANCAIRE_NOM\",.settled_at,.settled_at,.label,.reference]" | sed -r 's/"([0-9\-]+)T[0-9:\.]+Z"/\1/g' | sed -r "s/,\"([^,\"]+)\",\"($QONTO_COMPTEBANCAIRE_NOM)\",/,\1,\2,/" | sed 's/","/ /g' | sed -r 's/\[//' | sed 's/\]//' >> $HISTORY_FILE".new"
+curl -s -H "Authorization: $QONTO_IDENTIFIANT:$QONTO_SECRET" "https://thirdparty.qonto.com/v2/transactions?slug=$QONTO_IDENTIFIANT&iban=$QONTO_IBAN" | jq '.transactions[]' | jq -c "[.settled_at,.label,.reference,.side,.amount,.operation_type,\"$QONTO_COMPTEBANCAIRE_NOM\",.settled_at,.settled_at,.label,.reference]" | sed 's/,"debit",/,-/' | sed 's/,"credit",/,/' |  sed -r 's/"([0-9\-]+)T[0-9:\.]+Z"/\1/g' | sed -r "s/,\"([^,\"]+)\",\"($QONTO_COMPTEBANCAIRE_NOM)\",/,\1,\2,/" | sed 's/","/ /g' | sed -r 's/\[//' | sed 's/\]//' >> $HISTORY_FILE".new"
 
 grep "$QONTO_COMPTEBANCAIRE_NOM" $HISTORY_FILE | tail -n +15 > $HISTORY_FILE".old"
 grep -v "$QONTO_COMPTEBANCAIRE_NOM" $HISTORY_FILE >> $HISTORY_FILE".old"
