2381b8bde5097e6471297cf47d7e7e0fad3de4fc
Vincent LAURENT <vince.laurent@gmail.com>
2025-09-02 13:00:41 +0200

    Nom du compte en banque et nom de variable

--- bin/update_qonto.sh
+++ bin/update_qonto.sh
@@ -6,7 +6,8 @@ cd $(dirname $0)/..
 git pull > /dev/null
 
 QONTO_IBAN=$1
-QONTO_COMPTEBANCAIRE_NOM=$2
+QONTO_COMPTEBANCAIRE_KEY=$2
+QONTO_COMPTEBANCAIRE_NOM=$3
 
 HTTP_STATUS_CODE=$(curl -o /dev/null -s -w "%{http_code}\n" -H "Authorization: $QONTO_IDENTIFIANT:$QONTO_SECRET" "https://thirdparty.qonto.com/v2/transactions?slug=$QONTO_IDENTIFIANT&iban=$QONTO_IBAN")
 
@@ -17,10 +18,10 @@ fi
 
 echo "date,\"raw\",amount,type,id,rdate,vdate,\"label\"" > $HISTORY_FILE".new"
 
-curl -s -H "Authorization: $QONTO_IDENTIFIANT:$QONTO_SECRET" "https://thirdparty.qonto.com/v2/transactions?slug=$QONTO_IDENTIFIANT&iban=$QONTO_IBAN&status\[\]=completed&status\[\]=pending" | jq '.transactions[] | .label as $label | .reference |= gsub("^"+$label; "")' | jq -c "[.emitted_at,.label,.reference,.side,.amount,.operation_type,\"$QONTO_COMPTEBANCAIRE_NOM\",.emitted_at,.emitted_at,.label,.reference]" | sed 's/,"debit",/,-/' | sed 's/,"credit",/,/' |  sed -r 's/"([0-9\-]+)T[0-9:\.]+Z"/\1/g' | sed -r "s/,\"([^,\"]+)\",\"($QONTO_COMPTEBANCAIRE_NOM)\",/,\1,\2,/" | sed 's/,null/,""/g' | sed 's/","/ /g' | sed 's/ "/"/g' | sed -r 's/\[//' | sed 's/\]//' >> $HISTORY_FILE".new"
+curl -s -H "Authorization: $QONTO_IDENTIFIANT:$QONTO_SECRET" "https://thirdparty.qonto.com/v2/transactions?slug=$QONTO_IDENTIFIANT&iban=$QONTO_IBAN&status\[\]=completed&status\[\]=pending" | jq '.transactions[] | .label as $labelreplace | .reference |= gsub("^"+$labelreplace; "")' | jq -c "[.emitted_at,.label,.reference,.side,.amount,.operation_type,\"$QONTO_COMPTEBANCAIRE_KEY\",.emitted_at,.emitted_at,.label,.reference]" | sed 's/,"debit",/,-/' | sed 's/,"credit",/,/' |  sed -r 's/"([0-9\-]+)T[0-9:\.]+Z"/\1/g' | sed -r "s/,\"([^,\"]+)\",\"($QONTO_COMPTEBANCAIRE_KEY)\",/,\1,\2,/" | sed 's/,null/,""/g' | sed 's/","/ /g' | sed 's/ "/"/g' | sed -r 's/\[//' | sed 's/\]//' >> $HISTORY_FILE".new"
 
-grep "$QONTO_COMPTEBANCAIRE_NOM" $HISTORY_FILE | tail -n +15 > $HISTORY_FILE".old"
-grep -v "$QONTO_COMPTEBANCAIRE_NOM" $HISTORY_FILE >> $HISTORY_FILE".old"
+grep "$QONTO_COMPTEBANCAIRE_KEY" $HISTORY_FILE | tail -n +15 > $HISTORY_FILE".old"
+grep -v "$QONTO_COMPTEBANCAIRE_KEY" $HISTORY_FILE >> $HISTORY_FILE".old"
 cat $HISTORY_FILE".new" $HISTORY_FILE".old" | sed 's/,\(2[0-9-]*\),null,/,\1,\1,/' | awk -F "," 'BEGIN { FPAT = "([^,]+)|(\"[^\"]+\")"; OFS="," }{ gsub(",",".", $2); gsub("\\.0$","", $3); gsub(",",".",$8); print $0 }' | sort -t "," -k 1,3 -ur > $HISTORY_FILE".tmp"
 mv $HISTORY_FILE".tmp" $HISTORY_FILE
 rm -f $HISTORY_FILE".old" $HISTORY_FILE".new"
@@ -28,9 +29,9 @@ rm -f $HISTORY_FILE".old" $HISTORY_FILE".new"
 git add $HISTORY_FILE
 git commit -m "Mise à jour des opérations (qonto)" $HISTORY_FILE > /dev/null
 
-curl -s -H "Authorization: $QONTO_IDENTIFIANT:$QONTO_SECRET" "https://thirdparty.qonto.com/v2/bank_accounts" | jq ".bank_accounts[] | select(.iban==\"$QONTO_IBAN\")" | jq -c "[.name,.balance,.authorized_balance,.currency,\"\",\"$QONTO_COMPTEBANCAIRE_NOM\"]" | sed 's/"//g' | sed 's/\[//' | sed 's/\]//' > $LIST_FILE.tmp
+curl -s -H "Authorization: $QONTO_IDENTIFIANT:$QONTO_SECRET" "https://thirdparty.qonto.com/v2/bank_accounts" | jq ".bank_accounts[] | select(.iban==\"$QONTO_IBAN\")" | jq -c "[\"$QONTO_COMPTEBANCAIRE_NOM\",.balance,.authorized_balance,.currency,\"\",\"$QONTO_COMPTEBANCAIRE_KEY\"]" | sed 's/"//g' | sed 's/\[//' | sed 's/\]//' > $LIST_FILE.tmp
 
-cat $LIST_FILE | grep -v "^label" | grep -v "$QONTO_COMPTEBANCAIRE_NOM" >> $LIST_FILE.tmp
+cat $LIST_FILE | grep -v "^label" | grep -v "$QONTO_COMPTEBANCAIRE_KEY" >> $LIST_FILE.tmp
 
 echo "label,balance,coming,currency,type,id" > $LIST_FILE
 cat $LIST_FILE.tmp | sort >> $LIST_FILE
